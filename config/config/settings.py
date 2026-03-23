a5ed17a4ccc6128a34cf6d25e3b8825b2b45782a
denzel-builds <muleyadenzell@gmail.com>
2026-03-23 12:06:19 +0200

    Bad Gateway

--- config/config/settings.py
+++ config/config/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
